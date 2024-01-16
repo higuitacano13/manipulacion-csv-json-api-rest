61b85081006e5737731eb01141b059a9b44d7918
Juan Higuita <jhiguitac@gmail.com>
2024-01-15 20:19:24 -0500

    Finalización método GetCSVFileContentInJson

---
+++ WebAPI/ArchivoHelper.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.VisualBasic.FileIO;
+using Newtonsoft.Json;
+
+namespace WebAPI
+{
+    public class ArchivoHelper
+    {
+
+        public static List<string[]> LeerArchivoCSV(string rutaArchivo)
+        {
+            List<string[]> datosCSV = new List<string[]>();
+
+            // ----- Instrucciión using para liberar los recursos al finalizar de leer el archivo ---- //
+            using (TextFieldParser lectoArchivoCSV = new TextFieldParser(rutaArchivo))
+            {
+                // ----- Especificar archivo separado por coma ---- //
+                lectoArchivoCSV.TextFieldType = FieldType.Delimited;
+                lectoArchivoCSV.SetDelimiters(",");
+
+                while (!lectoArchivoCSV.EndOfData)
+                {
+                    string[] fila = lectoArchivoCSV.ReadFields(); // -> Leer Fila
+                    datosCSV.Add(fila);
+                }
+            }
+
+            return datosCSV;
+        }
+
+        public static string ConvertirDatosAJSON(List<string[]> datosCSV)
+        {
+            List<Dictionary<string, string>> listaDiccionarios = new List<Dictionary<string, string>>();
+
+            // ----- Creamos el encabezado con los titulos de el archivo CSV ---- //
+            string[] titulosTabla = datosCSV[0];
+
+            // ----- Rrecoremos a partir de la segunda fila ---- //
+            for (int i = 1; i < datosCSV.Count; i++)
+            {
+                string[] fila = datosCSV[i];
+                Dictionary<string, string> diccionario = new Dictionary<string, string>();
+
+                // -----  Guardamos en el diccionario ---- //
+                for (int j = 0; j < titulosTabla.Length && j < fila.Length; j++)
+                {
+                    diccionario[titulosTabla[j]] = fila[j];
+                }
+
+                listaDiccionarios.Add(diccionario);
+            }
+
+            // ----- Convertimos en formato Json ---- //
+            string jsonDatos = JsonConvert.SerializeObject(listaDiccionarios, Formatting.Indented);
+
+            return jsonDatos;
+        }
+    }
+}
---
+++ WebAPI/Controllers/ArchivoCSVController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualBasic.FileIO;
+using Newtonsoft.Json;
+
+namespace WebAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ArchivoCSVController : ControllerBase
+    {
+        [HttpGet(Name = "GetCSVFileContent")]
+        public IActionResult GetCSVFileContentInJson(string csvPath)
+        {
+            try
+            {
+                if (System.IO.File.Exists(csvPath))
+                {
+                    List<string[]> csvContent = ArchivoHelper.LeerArchivoCSV(csvPath);
+
+                    if (csvContent != null && csvContent.Count > 0)
+                    {
+                        string jsonData = ArchivoHelper.ConvertirDatosAJSON(csvContent);
+                        return Content(jsonData, "application/json");
+                    }
+                    else
+                    {
+                        return NotFound("El archivo CSV está vacío o no contiene datos válidos.");
+                    }
+                }
+                else
+                {
+                    return NotFound("El archivo CSV no fue encontrado en la ruta especificada.");
+                }
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Error interno del servidor: {ex.Message}");
+            }
+        }
+
+
+
+    }
+}
